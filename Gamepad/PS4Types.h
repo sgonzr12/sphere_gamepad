1bda441bfd71c5e01d1c1232570a1a0ebf5946cf
albenagrigorova <azdrag00@estudiantes.unileon.es>
2024-10-23 16:11:58 +0200

    inputs corregidos + physical maximum

--- Gamepad/PS4Types.h
+++ Gamepad/PS4Types.h
@@ -38,16 +38,16 @@ static uint8_t PSReportDescriptor[] = {
   0x15, 0x00,        //   Logical Minimum (0)
   0x25, 0x01,        //   Logical Maximum (1)
   0x35, 0x00,        //   Physical Minimum (0)
-  0x45, 0x01,        //   Physical Maximum (1)
+  0x46, 0x3B, 0x01,  //   Physical Maximum (315)    // 0x45, 0x01,        //   Physical Maximum (1)
   0x75, 0x01,        //   Report Size (1)
   0x95, 0x0E,        //   Report Count (14)
   0x05, 0x09,        //   Usage Page (Button)
   0x19, 0x01,        //   Usage Minimum (0x01)
   0x29, 0x0E,        //   Usage Maximum (0x0E)
-  0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,
+  0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,    //?
                      //   Preferred State,No Null Position)
   0x95, 0x02,        //   Report Count (2)
-  0x81, 0x01,        //   Input (Const,Array,Abs,No Wrap,Linear,
+  0x81, 0x02,        //   Input (Const,Array,Abs,No Wrap,Linear,    //
                      //   Preferred State,No Null Position)
   0x05, 0x01,        //   Usage Page (Generic Desktop Ctrls)
   0x25, 0x07,        //   Logical Maximum (7)
@@ -60,7 +60,7 @@ static uint8_t PSReportDescriptor[] = {
                      //   Preferred State,Null State)
   0x65, 0x00,        //   Unit (None)
   0x95, 0x01,        //   Report Count (1)
-  0x81, 0x01,        //   Input (Const,Array,Abs,No Wrap,Linear,
+  0x81, 0x02,        //   Input (Const,Array,Abs,No Wrap,Linear,    //?
                      //   Preferred State,No Null Position)
   0xC0,              // End Collection
 };
