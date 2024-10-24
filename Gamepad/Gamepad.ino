8994eb2637fb09e66eefe85786f6345582f86d68
sgonzr12 <sgonzr12@estudiantes.unileon.es>
2024-10-24 20:53:15 +0200

    feat: buttons threshold calibration

--- Gamepad/Gamepad.ino
+++ Gamepad/Gamepad.ino
@@ -42,6 +42,11 @@ void setup() {
   PS4GamepadReleaseAllButtons(&psGamepad);
 
   esp32USB.begin();
+
+  //calibrate the threshold values for all buttons
+  Serial.println("calibrating");
+  calibrateThresholdValues();
+
 }
 
 void loop() {
@@ -82,81 +87,86 @@ void loop() {
 
   //check if any button is pressed
   //imprimir valor del pin A
-  Serial.println(touchRead(BUTTON_ZR));
-  if (touchRead(BUTTON_B) > TOUCH_THRESHOLD) {
+  Serial.println(touchRead(1));
+  if (touchRead(BUTTON_B) > thresholdValues[BUTTON_B]) {
     PS4GamepadPressButton(B, &psGamepad);
-    Serial.println("Boton CROSS presionado");
+    Serial.println("Boton B presionado");
   } else {
     PS4GamepadReleaseButton(B, &psGamepad);
   }
-  if (touchRead(BUTTON_A) > TOUCH_THRESHOLD) {
+  if (touchRead(BUTTON_A) > thresholdValues[BUTTON_A]) {
     PS4GamepadPressButton(A, &psGamepad);
+    Serial.println("Boton A presionado");
   } else {
     PS4GamepadReleaseButton(A, &psGamepad);
   }
-  if (touchRead(BUTTON_Y) > TOUCH_THRESHOLD) {
+  if (touchRead(BUTTON_Y) > thresholdValues[BUTTON_Y]) {
     PS4GamepadPressButton(Y, &psGamepad);
+    Serial.println("Boton Y presionado");
   } else {
     PS4GamepadReleaseButton(Y, &psGamepad);
   }
-  if (touchRead(BUTTON_X) > TOUCH_THRESHOLD) {
+  if (touchRead(BUTTON_X) > thresholdValues[BUTTON_X]) {
     PS4GamepadPressButton(X, &psGamepad);
+    Serial.println("Boton X presionado");
   } else {
     PS4GamepadReleaseButton(X, &psGamepad);
   }
-  if (touchRead(BUTTON_L) > TOUCH_THRESHOLD) {
+  if (touchRead(BUTTON_L) > thresholdValues[BUTTON_L]) {
     Serial.println("Boton L1 presionado");
     PS4GamepadPressButton(L1, &psGamepad);
   } else {
     PS4GamepadReleaseButton(L1, &psGamepad);
   }
-  if (touchRead(BUTTON_R) > TOUCH_THRESHOLD) {
+  if (touchRead(BUTTON_R) > thresholdValues[BUTTON_R]) {
     Serial.println("Boton R1 presionado");
     PS4GamepadPressButton(R1, &psGamepad);
   } else {
     PS4GamepadReleaseButton(R1, &psGamepad);
   }
-  if (touchRead(BUTTON_ZL) > TOUCH_THRESHOLD) {
+  if (touchRead(BUTTON_ZL) > thresholdValues[BUTTON_ZL]) {
     Serial.println("Boton L2 presionado");
     PS4GamepadPressButton(L2, &psGamepad);
   } else {
     PS4GamepadReleaseButton(L2, &psGamepad);
   }
-  if (touchRead(BUTTON_ZR) > TOUCH_THRESHOLD) {
+  if (touchRead(BUTTON_ZR) > thresholdValues[BUTTON_ZR]) {
     Serial.println("Boton R2 presionado");
     PS4GamepadPressButton(R2, &psGamepad);
   } else {
     PS4GamepadReleaseButton(R2, &psGamepad);
   }
-  if (touchRead(BUTTON_HOME) > TOUCH_THRESHOLD) {
+  if (touchRead(BUTTON_HOME) > thresholdValues[BUTTON_HOME]) {
     PS4GamepadPressButton(OPTIONS, &psGamepad);
+    Serial.println("Boton OPTIONS presionado");
   } else {
     PS4GamepadReleaseButton(OPTIONS, &psGamepad);
   }
-  if (touchRead(BUTTON_CAPTURE) > TOUCH_THRESHOLD) {
+  if (touchRead(BUTTON_CAPTURE) > thresholdValues[BUTTON_CAPTURE]) {
     PS4GamepadPressButton(SHARE, &psGamepad);
+    Serial.println("Boton SHARE presionado");
   } else {
     PS4GamepadReleaseButton(SHARE, &psGamepad);
   }
-  if (touchRead(DPAD_UP) > TOUCH_THRESHOLD) {
+  if (touchRead(DPAD_UP) > thresholdValues[DPAD_UP]) {
     Serial.println("Boton DPAD-U presionado");
     PS4GamepadPressButton(UP, &psGamepad);
   } else {
     PS4GamepadReleaseButton(UP, &psGamepad);
   }
-  if (touchRead(DPAD_DOWN) > TOUCH_THRESHOLD) {
+  if (touchRead(DPAD_DOWN) > thresholdValues[DPAD_DOWN]) {
     Serial.println("Boton DPAD-D presionado");
     PS4GamepadPressButton(DOWN, &psGamepad);
   } else {
     PS4GamepadReleaseButton(DOWN, &psGamepad);
   }
-  if (touchRead(DPAD_LEFT) > TOUCH_THRESHOLD) {
+  if (touchRead(DPAD_LEFT) > thresholdValues[DPAD_LEFT]) {
     Serial.println("Boton DPAD-L presionado");
     PS4GamepadPressButton(LEFT, &psGamepad);
   } else {
     PS4GamepadReleaseButton(LEFT, &psGamepad);
   }
-  if (touchRead(DPAD_RIGHT) > TOUCH_THRESHOLD) {
+  if (touchRead(DPAD_RIGHT) > thresholdValues[DPAD_RIGHT]) {
     Serial.println("Boton DPAD-R presionado");
     PS4GamepadPressButton(RIGHT, &psGamepad);
   } else {
@@ -171,4 +181,38 @@ void loop() {
 
   // Añadir un delay para que no se envíen los datos tan rápido
   delay(100);
+}
+
+void calibrateThresholdValues() {
+  //calibrate the threshold values for all buttons
+
+  Serial.println("calibrating threshold values for all buttons");
+  int values[BUTTONSNUMBER+1];
+  unsigned long tiempoinicio = millis();
+  int counter = 0;
+
+  // Inicializar valores
+  for(int i = 0; i <= BUTTONSNUMBER; i++){
+    values[i] = 0;
+  }
+
+  //obtener valores
+  while(millis() - tiempoinicio < calibrationTime){
+    for(int i = 1; i <= BUTTONSNUMBER; i++){
+      values[i] += touchRead(i);
+    }
+    counter ++;
+  }
+
+  //calcular promedio
+  for(int i = 1; i <= BUTTONSNUMBER; i++){
+    thresholdValues[i] = (values[i] / counter)*TOUCHED_THRESHOLD_FACTOR;
+
+    //imprimir valor
+    Serial.print("Threshold Boton ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(thresholdValues[i]);
+  }
+
 }
--- Gamepad/Pins.h
+++ Gamepad/Pins.h
@@ -1,19 +1,31 @@
 
+
+//declaration of number of buttons in the gamepad
+const int BUTTONSNUMBER = 14; 
+
 //declaration for the touch pins for all buttons
-int BUTTON_A = 15;
-int BUTTON_B = 15;
-int BUTTON_X = 15;
-int BUTTON_Y = 15;
+int BUTTON_A = 1;
+int BUTTON_B = 2;
+int BUTTON_X = 3;
+int BUTTON_Y = 4;
 int BUTTON_L = 5;
-int BUTTON_R = 1;
-int BUTTON_ZL = 4;
-int BUTTON_ZR = 2;
-int BUTTON_HOME = 15;
-int BUTTON_CAPTURE = 15;
-int DPAD_UP = 15;
-int DPAD_DOWN = 15;
-int DPAD_LEFT = 15;
-int DPAD_RIGHT = 15;
+int BUTTON_R = 6;
+int BUTTON_ZL = 7;
+int BUTTON_ZR = 8;
+int BUTTON_HOME = 9;
+int BUTTON_CAPTURE = 10;
+int DPAD_UP = 11;
+int DPAD_DOWN = 12;
+int DPAD_LEFT = 13;
+int DPAD_RIGHT = 14;
 
+//declaration of the calibration time
+int calibrationTime = 2000;
 
-int TOUCH_THRESHOLD = 10000000;
+//declaration of the array of threshold values for all buttons
+int thresholdValues[BUTTONSNUMBER+1];
+
+#define TOUCHED_THRESHOLD_FACTOR 1.20
+
+//declaration of the touch threshold
+//int TOUCH_THRESHOLD = 100000;
